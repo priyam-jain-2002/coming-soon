45f011ef2c6169d54f41ed21f6c0db844f05c86c
Priyam-jain123 <priyamjain1181@gmail.com>
2023-07-03 19:51:32 +0530

    updated file

--- dist/js/scripts.js
+++ dist/js/scripts.js
@@ -1,7 +1,7 @@
 /*!
-* Start Bootstrap - Coming Soon v6.0.7 (https://startbootstrap.com/theme/coming-soon)
-* Copyright 2013-2023 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-coming-soon/blob/master/LICENSE)
+* Start Bootstrap - Quarks Industrials Coming Soon v0.1 (https://quarksindustrials.com)
+* Copyright 2013-2023 Priyam Jain
+* Licensed under MIT (https://github.com/priyam-jain-2002/quarksindustrials/blob/master/LICENSE)
 */
 // This file is intentionally blank
 // Use this file to add JavaScript to your project
